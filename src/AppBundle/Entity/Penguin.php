dc8fb48e8e04a5d8ba8feb3e5eb6d79dee5308e8
igor <igor.sv.n@gmail.com>
2016-07-05 10:27:28 +0300

    Implement penguin base entity class.

---
+++ src/AppBundle/Entity/Penguin.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2016, Igor Nikolaev
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Penguin
+ *
+ * @ORM\Entity
+ */
+class Penguin
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", unique=true)
+     * @ORM\GeneratedValue(strategy="UUID")
+     * @ORM\Id
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank
+     */
+    private $title;
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $title title
+     *
+     * @return Penguin
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
