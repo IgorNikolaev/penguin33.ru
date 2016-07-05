9db82fcf0ef7373fc701cf9b83bfa17b43e9c257
igor <igor.sv.n@gmail.com>
2016-07-05 10:16:39 +0300

    Add doc comments to the AppBundle class.

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -1,9 +1,20 @@
 <?php
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2016, Igor Nikolaev
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * App bundle
+ */
 class AppBundle extends Bundle
 {
+
 }
